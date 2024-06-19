1989d4b6a6577b4704573d1d01719562d31ddc31
Abhinav Shrivastav <abhinav312003@gmail.com>
2024-06-19 19:04:17 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -4,10 +4,9 @@ const router = require("./router/auth-router");
 const connectDb = require("../server/utils/db");
 const contactRoute = require("./router/contact-router");
 const cors = require('cors');
+
+
 app.use(cors());
-
-
-
 app.use(express.json());
 app.use('/api/v1', router);
 app.use('/api/form', contactRoute); // Added a forward slash (/) before 'api/form'
